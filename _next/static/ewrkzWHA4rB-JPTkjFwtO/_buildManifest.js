f89ec52376b90cf0ba0e33b618745dbeed2a2792
Hanyu Tang <hanyutang.sandra@gmail.com>
2025-02-02 21:48:03 -0500

    Updates

--- _next/static/ewrkzWHA4rB-JPTkjFwtO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/675-ac4880823431a640.js","static/chunks/pages/index-5dfe7d2513472158.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
